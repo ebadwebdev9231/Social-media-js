362ac83b47dc384e2fd508267f954016ccfa3977
ebadwebdev9231 <kebad9231@gmail.com>
2024-04-18 17:34:13 +0500

    Create Header, Footer, and Sidebar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="d-flex">
+      <Sidebar />
+      <div className="w-100">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
